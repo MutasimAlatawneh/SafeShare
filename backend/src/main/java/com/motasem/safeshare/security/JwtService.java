fabd8919f35e7a91a8aee5ee95818d9ba0746960
Motasem Alatawna <mutasim.alatawneh@gmail.com>
2026-06-02 18:46:10 +0300

    chore: explicitly set JWT expiration to 30 minutes

--- backend/src/main/java/com/motasem/safeshare/security/JwtService.java
+++ backend/src/main/java/com/motasem/safeshare/security/JwtService.java
@@ -40,7 +40,7 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // Fixed: 24 Hours
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30)) // 30 Minutes
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
